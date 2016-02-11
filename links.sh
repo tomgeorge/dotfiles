aaab17661ec456eef72925f6434d3ebf3bfd163a
Tom George <tg82490@gmail.com>
2016-02-11 23:08:03 +0000

    trying ~

--- links.sh
+++ links.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-rm /home/$(whoami)/.zshrc || true
-ln /home/vagrant/dotfiles/zsh/.zshrc /home/vagrant/.zshrc
-rm /home/$(whoami)/.tmux.conf || true
-ln /home/vagrant/dotfiles/tmux/.tmux.conf /home/vagrant/.tmux.conf
-rm /home/$(whoami)/.vimrc || true
-ln /home/vagrant/dotfiles/vim/.vimrc /home/vagrant/.vimrc
-rm /home/$(whoami)/.bashrc || true
-ln /home/vagrant/dotfiles/bash/.bashrc /home/vagrant/.bashrc
-rm /home/$(whoami)/.gitconfig || true
-ln /home/vagrant/dotfiles/git/.gitconfig /home/vagrant/.gitconfig || true       
+rm ~/.zshrc || true
+ln ~/dotfiles/zsh/.zshrc ~/.zshrc
+rm ~/.tmux.conf || true
+ln ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+rm ~/.vimrc || true
+ln ~/dotfiles/vim/.vimrc ~/.vimrc
+rm ~/.bashrc || true
+ln ~/dotfiles/bash/.bashrc ~/.bashrc
+rm ~/.gitconfig || true
+ln ~/dotfiles/git/.gitconfig ~/.gitconfig || true       
