4e5fa81a4847751bd7910e39146bd7d3a20ac843
Tom George <tg82490@gmail.com>
2016-02-16 17:45:13 +0000

    fix ln commands

--- links-vagrant.sh
+++ links-vagrant.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 rm /home/vagrant/.zshrc || true
-ln /home/vagrant/dotfiles/zshrc ~/.zshrc
+ln /home/vagrant/dotfiles/zshrc /home/vagrant/.zshrc
 rm /home/vagrant/.tmux.conf || true
-ln /home/vagrant/dotfiles/.tmux.conf ~/.tmux.conf
+ln /home/vagrant/dotfiles/tmux.conf /home/vagrant/.tmux.conf
 rm /home/vagrant/.vimrc || true
-ln /home/vagrant/dotfiles/.vimrc ~/.vimrc
+ln /home/vagrant/dotfiles/vimrc /home/vagrant/.vimrc
 rm /home/vagrant/.bashrc || true
-ln /home/vagrant/dotfiles/.bashrc ~/.bashrc
+ln /home/vagrant/dotfiles/bashrc /home/vagrant/.bashrc
 rm /home/vagrant/.gitconfig || true
-ln /home/vagrant/dotfiles/.gitconfig ~/.gitconfig || true       
+ln /home/vagrant/dotfiles/gitconfig /home/vagrant/.gitconfig || true       
