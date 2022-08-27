c4d082ec7b6b20010ef08ba05643a9a3bbbe3519
Tom George <tg82490@gmail.com>
2022-08-27 17:38:36 -0500

    Add gitsign options to codeactions :tada:

--- nvim/lua/modules/tools/null-ls-nvim/init.lua
+++ nvim/lua/modules/tools/null-ls-nvim/init.lua
@@ -7,5 +7,6 @@ null_ls.setup {
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.code_actions.gitsigns,
   },
 }
