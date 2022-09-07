cd06736d013fb6e8d126cfad235a82aa5314f1d7
Tom George <tg82490@gmail.com>
2022-09-06 20:22:44 -0500

    Re-enable the zip plugins so I can e.g. goto def of JARs

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -26,11 +26,11 @@ end
 createdir()
 
 --disable_distribution_plugins
-vim.g.loaded_gzip = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
+-- vim.g.loaded_gzip = 1
+-- vim.g.loaded_tar = 1
+-- vim.g.loaded_tarPlugin = 1
+-- vim.g.loaded_zip = 1
+-- vim.g.loaded_zipPlugin = 1
 vim.g.loaded_getscript = 1
 vim.g.loaded_getscriptPlugin = 1
 vim.g.loaded_vimball = 1
