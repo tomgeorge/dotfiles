2d3adaf8b2f6aedf5e341ff39d4763349d9b8fe4
Tom George <tom.george@procore.com>
2023-01-26 17:06:19 -0600

    Add shellcheck

--- nvim/lua/modules/tools/null-ls-nvim/init.lua
+++ nvim/lua/modules/tools/null-ls-nvim/init.lua
@@ -8,6 +8,8 @@ null_ls.setup {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.code_actions.shellcheck,
+    null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.clj_kondo,
   },
 }
