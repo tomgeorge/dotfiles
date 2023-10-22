4b6e8ffe0b86698e112f910a744056032fada698
Tom George <tom.george@procore.com>
2023-10-22 12:59:08 -0500

    Need cmp-luasnip to get snippets as a completion source

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -64,6 +64,7 @@ local plugins = {
       "hrsh7th/cmp-nvim-lsp-signature-help",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
+      "saadparwaiz1/cmp_luasnip",
       {
         "L3MON4D3/LuaSnip",
         dependencies = {
