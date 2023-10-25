a8d7193faa2a99a41bda07e2d95269d1a21303ae
Tom George <tom.george@procore.com>
2023-10-25 09:53:23 -0500

    Telescope buffers sort by last used

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -24,7 +24,14 @@ M.telescope = {
     ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
     ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
-    ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
+    ["<leader>fb"] = {
+      function()
+        require("telescope.builtin").buffers({
+          sort_lastused = true,
+          sort_mru = true,
+        })
+      end,
+    },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
     ["<leader>fz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
