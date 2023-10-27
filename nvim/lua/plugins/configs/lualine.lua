97fd3de3596b304f5c104a3890402f72f43e5bd0
Tom George <tom.george@procore.com>
2023-10-27 00:02:49 -0500

    Different glyph for LSP

--- nvim/lua/plugins/configs/lualine.lua
+++ nvim/lua/plugins/configs/lualine.lua
@@ -6,10 +6,10 @@ M.status_line = function()
       local active_clients = vim.lsp.get_clients()
       local client_count = #active_clients
       if #messages > 0 then
-        return " LSP:" .. client_count .. " " .. table.concat(messages, " ")
+        return "󱁤 LSP:" .. client_count .. " " .. table.concat(messages, " ")
       end
       if #active_clients <= 0 then
-        return " LSP:" .. client_count
+        return "󱁤 LSP:" .. client_count
       else
         local client_names = {}
         for i, client in ipairs(active_clients) do
@@ -17,7 +17,7 @@ M.status_line = function()
             table.insert(client_names, client.name)
           end
         end
-        return " LSP:" .. client_count .. " [" .. table.concat(client_names, " ") .. "]"
+        return "󱁤 LSP:" .. client_count .. " [" .. table.concat(client_names, " ") .. "]"
       end
     end,
   })
