1fa93bf404d800d13932f2e0907f2954bd60d084
Tom George <tom.george@procore.com>
2023-11-14 16:04:25 -0600

    Conjure:
    - Disable raw output
    - Change HUD position
    - Disable auto-repl
    
    Signed-off-by: Tom George <tom.george@procore.com>

--- nvim/after/ftplugin/clojure.lua
+++ nvim/after/ftplugin/clojure.lua
@@ -1,6 +1,8 @@
 vim.g.maplocalleader = ","
-vim.g["conjure#log#hud#anchor"] = "SE"
+vim.g["conjure#log#hud#anchor"] = "NE"
 vim.g["conjure#log#hud#border"] = "none"
 vim.g["conjure#mapping#doc_word"] = nil
+vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
+vim.g["conjure#client#clojure#nrepl#eval#raw_out"] = true
 
 vim.keymap.set("n", "C-]", "<Cmd>ConjureDefWord<CR>", { desc = "Conjure" })
