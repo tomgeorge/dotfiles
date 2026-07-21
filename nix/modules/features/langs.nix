5455eca330be83cb59f8cbc9d414eb367d6ef4b3
Tom George <thomas.george@chainguard.dev>
2026-07-21 10:58:48 -0400

    balena linker crash

--- nix/modules/features/langs.nix
+++ nix/modules/features/langs.nix
@@ -6,7 +6,7 @@
         home.packages = with pkgs; [
           apko
           babashka
-          balena-cli
+          # balena-cli # ld64 fortify hardening crash; re-enable once nixos/nixpkgs#536365 reaches nixpkgs-unstable
           cmake
           cosign
           erlang
